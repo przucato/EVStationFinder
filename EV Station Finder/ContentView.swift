243a31e1c89b4d8e650990ac476307a4d82e83c7
Priscila <priscila.zucato@usp.br>
2025-06-28 18:32:07 -0300

    Initial Commit

---
+++ EV Station Finder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EV Station Finder
+//
+//  Created by Priscila Zucato on 28/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EV Station Finder/EV_Station_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EV_Station_FinderApp.swift
+//  EV Station Finder
+//
+//  Created by Priscila Zucato on 28/06/25.
+//
+
+import SwiftUI
+
+@main
+struct EV_Station_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
